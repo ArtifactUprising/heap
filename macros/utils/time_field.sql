2d2c1ac46e426f0c68553dbd6688a7e58b2f93bf
Dylan Baker <dylang.baker@googlemail.com>
2021-05-28 10:49:42 -0400

    Update time_field.sql

--- macros/utils/time_field.sql
+++ macros/utils/time_field.sql
@@ -1,6 +1,6 @@
 {% macro time_field(field_name) %}
 
-    {{ return(adapter.dispatch('time_field', packages = dbt_utils._get_utils_namespaces() + ['heap'])()) }}
+    {{ return(adapter.dispatch('time_field', packages = dbt_utils._get_utils_namespaces() + ['heap'])(field_name)) }}
 
 {% endmacro %}
 
